d755cf45fe819de94779d095affe7ed26cae8969
Narendra Lama <narendra.lama.tech@gmail.com>
2025-10-08 09:28:33 +1100

    Fixing the bug

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import UserCard from "./UserCard";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const feed = useSelector((store) => store.feed);
+
+  const dispatch = useDispatch();
+
+  const getFeed = async () => {
+    // Only skip if feed already has data
+    if (feed && feed.length > 0) return;
+
+    try {
+      console.log("Fetching feed from:", BASE_URL + "/feed");
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+
+      dispatch(addFeed(res.data.users));
+    } catch (err) {
+      console.error("Feed error:", err);
+      console.error("Error response:", err.response?.data);
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  // Handle loading state
+  if (!feed) {
+    return <div className="flex justify-center my-10">Loading...</div>;
+  }
+
+  // Handle empty feed
+  if (feed.length === 0) {
+    return <div className="flex justify-center my-10">No users found</div>;
+  }
+
+  return (
+    <div className="flex justify-center my-10">
+      <UserCard user={feed[0]} />
+    </div>
+  );
 };
 
 export default Feed;
