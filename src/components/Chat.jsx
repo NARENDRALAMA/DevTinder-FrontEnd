c7741ce7272d0fb9d6ead87d5dbfd361982d4ae6
Narendra Lama <narendra.lama.tech@gmail.com>
2025-12-15 09:51:10 +1100

    refactor Chat component to integrate user ID and fix socket connection

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,15 +3,20 @@ import { useParams } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
 import { createSocketConnection } from "../utils/socket";
+import { useSelector } from "react-redux";
 
 const Chat = () => {
   const { targetUserId } = useParams();
   const [messages, setMessages] = useState([{ text: "Hello World!" }]);
 
+  const user = useSelector((store) => store.user);
+
+  const userId = user?._id;
+
   useEffect(() => {
     const socket = createSocketConnection();
-    socket.emait("joinRoom", { targetUserId });
-  });
+    socket.emit("joinChat", { userId, targetUserId });
+  }, []);
   console.log(targetUserId);
   return (
     <div className="w-1/2 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col">
@@ -19,7 +24,7 @@ const Chat = () => {
       <div className="flex-1 overflow-scroll p-5">
         {messages.map((msg, index) => {
           return (
-            <div>
+            <div key={index}>
               <div className="chat chat-start">
                 <div className="chat-header">
                   Narendra Lama
